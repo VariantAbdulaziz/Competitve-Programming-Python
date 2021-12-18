3a7ceae4ddf5ca551da7c2e7eed0b38ca6697e46
Abdulaziz Ali <s9uidward@gmail.com>
2021-12-18 22:47:03 +0300

    minor changes on the comments

--- k_closest_points.py
+++ k_closest_points.py
@@ -1,9 +1,4 @@
 # solution for https://leetcode.com/problems/k-closest-points-to-origin/submissions/
-# for this particular question i chose bubble sort as it will allow me
-# to stop sorting half way through - i,e, until the specified k
-# also i want to make the sort backward in order for the smallest numbers
-# to build up at the start
-
 
 class Solution:
     def eucliDistance(self, point):
