cbe38c32e327a4c9b51e14695fb4407aa9360db5
Abdulaziz Ali <s9uidward@gmail.com>
2021-12-19 11:04:16 +0300

    solution to https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/submissions/

--- less_than_me.py
+++ less_than_me.py
@@ -1,28 +1,19 @@
-# solution to https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
-
-# for this particular question i chose to sort the items in a
-# non decreasing order with selectin sort as it maitains the index
-# that the selection is coming from 
+ # solution to https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/submissions/
 
 class Solution:
-    def select(self, nums, i):
-        j = i + 1
-        while j < len(nums):
-            if nums[j] > nums[i]:
-                i = j
-            j += 1
-            
-        return i
-    def smallerNumbersThanCurrent(self, nums):
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        lessThanfrequencyArray = [0] * 101
         lessThanCount = [0] * len(nums)
         
-        i = 0
-        while i < len(nums) - 1:
-            j = self.select(nums, i)
-            nums[i], nums[j] = nums[j], nums[i]
-            lessThanCount[j] = len(nums) - 1 - i
-                
-                
-            i += 1
-            
+        for elem in nums:
+            i = elem + 1
+            while i < 101:
+                lessThanfrequencyArray[i] += 1
+                i += 1
+        
+        for i in range(0, len(nums)):
+            lessThanCount[i] = lessThanfrequencyArray[nums[i]]
+        
+        
         return lessThanCount
+        
