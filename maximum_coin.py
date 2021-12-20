c34609ea0efbe156a9bdef8d3202dc40296f291b
Abdulaziz Ali <s9uidward@gmail.com>
2021-12-20 16:29:09 +0300

    solution to https://leetcode.com/problems/maximum-number-of-coins-you-can-get/submissions/

---
+++ maximum_coin.py
@@ -0,0 +1,12 @@
+# solution to https://leetcode.com/problems/maximum-number-of-coins-you-can-get/submissions/
+
+class Solution:
+    def maxCoins(self, piles):
+        
+        piles.sort(reverse = True)
+        
+        result = 0
+        for i in range(0, 2 * len(piles)//3, 2):
+            result += piles[i + 1]
+            
+        return result
