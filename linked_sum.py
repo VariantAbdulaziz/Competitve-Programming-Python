bf9ff79b6127fb550cc4673f7ece10ca517c17a6
Abdulaziz Ali <s9uidward@gmail.com>
2022-04-16 19:58:02 +0300

    correcting linked sum

--- linked_sum.py
+++ linked_sum.py
@@ -1,36 +1,27 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-def addTwoNumbers(l1, l2):
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         sum_head = ListNode()
-        remain = 0
         cur_sum = sum_head
         before_cur  = None
         while l1 != None or l2 != None:
             left = l1.val if l1 != None else 0
             right = l2.val if l2 != None else 0
-
-            cur_sum.val = remain + (left + right) % 10
+            
             cur_sum.next = ListNode()
-            cur_sum.next.val = remain = (left + right) // 10
+            cur_sum.next.val = (cur_sum.val + left + right) // 10
+            cur_sum.val = (cur_sum.val + left + right) % 10
             before_cur = cur_sum
             cur_sum = cur_sum.next
 
-            l1 = l1.next
-            l2 = l2.next
+            l1 = l1.next if l1 != None else None
+            l2 = l2.next if l2 != None else None
 
-        if remain == 0:
+        if cur_sum.val == 0:
             before_cur.next = None
             
         return sum_head
-
-l1 = ListNode(3)
-
-l2 = ListNode(4)
-sum = addTwoNumbers(l1, l2)
-
-while sum != None:
-    print(sum.val)
-    sum = sum.next
